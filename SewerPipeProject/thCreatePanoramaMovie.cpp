13cdbbc2018ed2f26a60d51cf7a7db7c57ef3181
HayashiTomoki <b15725bb@aitech.ac.jp>
2015-07-23 10:58:09 +0900

    [add]ヘッダファイルとソースファイルの追加
    パノラマ映像生成用のヘッダファイルとソースファイルの追加

---
+++ SewerPipeProject/thCreatePanoramaMovie.cpp
@@ -0,0 +1,10 @@
+//
+//  thCreatePanoramaMovie.cpp
+//  SewerPipeProject
+//
+//  Created by HayashiTomoki on 2015/07/23.
+//  Copyright (c) 2015年 HayashiTomoki. All rights reserved.
+//
+
+#include "thCreatePanoramaMovie.h"
+
---
+++ SewerPipeProject/thCreatePanoramaMovie.h
@@ -0,0 +1,14 @@
+//
+//  thCreatePanoramaMovie.h
+//  SewerPipeProject
+//
+//  Created by HayashiTomoki on 2015/07/23.
+//  Copyright (c) 2015年 HayashiTomoki. All rights reserved.
+//
+
+#ifndef __SewerPipeProject__thCreatePanoramaMovie__
+#define __SewerPipeProject__thCreatePanoramaMovie__
+
+#include <stdio.h>
+
+#endif /* defined(__SewerPipeProject__thCreatePanoramaMovie__) */
