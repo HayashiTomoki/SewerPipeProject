04b2b90400fd77dff6c3752dc58b5b87ee746ef5
HayashiTomoki <b15725bb@aitech.ac.jp>
2015-07-23 10:32:40 +0900

    Initial Commit

---
+++ SewerPipeProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SewerPipeProject
+//
+//  Created by HayashiTomoki on 2015/07/23.
+//  Copyright (c) 2015年 HayashiTomoki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
